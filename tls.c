pizhenwei/atophttpd

commit 6a44eb6458a1d18c17ebdf00b92fa1cd0ef75b8e

fix tls stackoverflow
Signed-off-by: Meinhard Zhou <zhouenhua@bytedance.com>

tls.c
@@ -208,9 +208,14 @@ static int conn_writev_tls(connection* conn, const struct iovec* iov, int iovcnt
 		iov_bytes_len += iov[i].iov_len;
 	}
 
-	char ssl_buf[iov_bytes_len];
+	char *ssl_buf;
+	ssl_buf = malloc(iov_bytes_len);
+	if (!ssl_buf) {
+		perror("SSL have no mem");
+		return -ENOMEM;
+	}
 	size_t offset = 0;
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < iovcnt; i++) {
 		memcpy(ssl_buf + offset, iov[i].iov_base, iov[i].iov_len);
 		offset += iov[i].iov_len;
 	}

commit d9678d074e839ba38e0dcd088f83d6e01e146255

Fix memory leak for SSL writev
Fixes: 6a44eb6458a("fix tls stackoverflow")
Signed-off-by: zhenwei pi <pizhenwei@bytedance.com>

tls.c
@@ -223,10 +223,11 @@ static int conn_writev_tls(connection* conn, const struct iovec* iov, int iovcnt
 	int ret = SSL_write(tls_conn->ssl, ssl_buf, iov_bytes_len);
 	if (ret < 0) {
 		perror("SSL writev error");
-		return -errno;
+		ret = -errno;
 	}
 
-	return 0;
+	free(ssl_buf);
+	return ret;
 }
 
 static int conn_read_tls(connection* conn, void* buf, size_t buf_len) {